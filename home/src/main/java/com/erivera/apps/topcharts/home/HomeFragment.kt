c86d366b00a41009251806b4afdd4c2dd79ca150
Erick Rivera <erickrivera2012@gmail.com>
2021-06-27 18:19:04 -0700

    Add home module

--- home/src/main/java/com/erivera/apps/topcharts/home/HomeFragment.kt
+++ home/src/main/java/com/erivera/apps/topcharts/home/HomeFragment.kt
@@ -1,16 +1,15 @@
-package com.erivera.apps.topcharts.ui.fragment
+package com.erivera.apps.topcharts.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.erivera.apps.topcharts.databinding.FragmentHomeBinding
+import com.erivera.apps.topcharts.home.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
